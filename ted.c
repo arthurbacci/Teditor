9cc84087cbbe0b424b445246f3e20b4d270dbfb5
Arthur Bacci <arthurbacci@protonmail.com>
2021-01-31 19:11:43 -0300

    Now you can change the linebreak type with Ctrl-H

--- ted.c
+++ ted.c
@@ -212,6 +212,38 @@ void config_dialog()
             free(answer1);
         }
     }
+    else if (strcmp(answer, "linebreak") == 0)
+    {
+        char msg1[1000];
+        snprintf(msg1, 1000, "linebreak (LF, CR, CRLF): ");
+
+        char *answer1 = prompt(msg1);
+
+        if (answer1 == NULL)
+        {
+            message("Canceled");
+        }
+        else {
+            if (strcmp(answer1, "LF") == 0)
+            {
+                config.line_break_type = 0;
+            }
+            else if (strcmp(answer1, "CRLF") == 0)
+            {
+                config.line_break_type = 1;
+            }
+            else if (strcmp(answer1, "CR") == 0)
+            {
+                config.line_break_type = 2;
+            }
+            else
+            {
+                message("Needs to be LF, CRLF or CR");
+            }
+
+            free(answer1);
+        }
+    }
     else {
         char msg1[1000];
         snprintf(msg1, 1000, "This option does not exist");
