569b1ee6e2a4ffd52ae986dbaf5c6a80d56c2847
Arthur Bacci <arthurbacci@protonmail.com>
2021-02-08 13:55:10 -0300

    Reformated free.c

--- src/free.c
+++ src/free.c
@@ -1,9 +1,7 @@
 #include "ted.h"
 
-void free_lines()
-{
-    for (unsigned int i = 0; i < num_lines; i++)
-    {
+void free_lines() {
+    for (unsigned int i = 0; i < num_lines; i++) {
         free(lines[i].data);
         lines[i].len = 0;
     }
