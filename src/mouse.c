11fbb6b1a3e8afeb25099322e2ca67f989d617a4
Arthur Bacci <arthurbacci@protonmail.com>
2021-02-12 11:03:48 -0300

    Add horizontal mouse behavior

--- src/mouse.c
+++ src/mouse.c
@@ -3,6 +3,10 @@
 void processMouseEvent(MEVENT ev) {
     if (ev.bstate & BUTTON1_CLICKED || ev.bstate & BUTTON1_PRESSED) {
         cursor.y = text_scroll.y + ev.y;
+        if ((unsigned int)ev.x >= len_line_number + 1)
+            cursor.x = text_scroll.x + ev.x - len_line_number - 1;
+        else
+            cursor.x = 0;
         cursor_in_valid_position();
     }
 }
