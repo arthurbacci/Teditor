19e49786fd813083c6e16c4ef049c9107a51af0d
Arthur Bacci <arthurbacci@protonmail.com>
2021-02-14 15:05:31 -0300

    Add syntax highlight

--- src/color.c
+++ src/color.c
@@ -1,22 +1,26 @@
 #include "ted.h"
 
 void syntaxHighlight(unsigned int at) {
+    const char *strings[] = {"if", "else", "int ", "char ", "*", "\"", "\'", "//", "unsigned ", "long ", "double ", "float ", "struct ", "const ", "return", ";"};
+    unsigned int slen = sizeof(strings) / sizeof(char *);
     for (unsigned int i = 0; i < lines[at].length; i++) {
         lines[at].color[i] = 0x0;
-        const char *string = "if";
-        if (lines[at].length - i < strlen(string))
-            continue;
-        bool c = 0;
-        for (unsigned int j = 0; j < strlen(string); j++)
-            if ((uchar32_t)string[j] != lines[at].data[i + j]) {
-                c = 1;
-                break;
-            }
-        if (c)
-            continue;
-        for (unsigned int j = 0; j < strlen(string); j++)
-            lines[at].color[i + j] = 0x10;
-        i += strlen(string) - 1;
+        for (unsigned int k = 0; k < slen; k++) {
+            if (lines[at].length - i < strlen(strings[k]))
+                continue;
+            bool c = 0;
+            for (unsigned int j = 0; j < strlen(strings[k]); j++)
+                if ((uchar32_t)strings[k][j] != lines[at].data[i + j]) {
+                    c = 1;
+                    break;
+                }
+            if (c)
+                continue;
+            for (unsigned int j = 0; j < strlen(strings[k]); j++)
+                lines[at].color[i + j] = 0x10;
+            i += strlen(strings[k]) - 1;
+            break;
+        }
     }
 }
 
