fafe8d1dab0fa4628941bc7ef71e72a29c15cd6a
Arthur Bacci <arthurbacci@protonmail.com>
2024-10-27 14:19:57 -0300

    new python code indenting style

--- plugins/ted_editorconfig.py
+++ plugins/ted_editorconfig.py
@@ -7,42 +7,42 @@ from sys import exit
 from editorconfig import get_properties
 
 if len(argv) != 3:
-    exit(102)
+  exit(102)
 
 try:
-    filename = abspath(argv[2])
+  filename = abspath(argv[2])
 except:
-    exit(103)
+  exit(103)
 
 try:
-    props = get_properties(filename)
+  props = get_properties(filename)
 except:
-    exit(104)
+  exit(104)
 
 try:
-    ret = props[argv[1]]
+  ret = props[argv[1]]
 except:
-    exit(101)
+  exit(101)
 
 
 match argv[1]:
-    case 'indent_style':
-        if ret == 'tab':
-            exit(1)
-        elif ret == 'space':
-            exit(2)
-    case 'indent_size':
-        if ret == 'tab':
-            exit(0)
-        elif int(ret) < 100:
-            exit(int(ret))
-        else:
-            exit(99)
-    case 'tab_width':
-        if int(ret) < 100:
-            exit(int(ret))
-        else:
-            exit(99)
+  case 'indent_style':
+    if ret == 'tab':
+      exit(1)
+    elif ret == 'space':
+      exit(2)
+  case 'indent_size':
+    if ret == 'tab':
+      exit(0)
+    elif int(ret) < 100:
+      exit(int(ret))
+    else:
+      exit(99)
+  case 'tab_width':
+    if int(ret) < 100:
+      exit(int(ret))
+    else:
+      exit(99)
 
 
 
